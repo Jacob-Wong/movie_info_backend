4b9ac1e4c8f6997ed5a48a4f77242e41cc45ce7c
Jacob-Wong <jacobwfk@gmail.com>
2021-05-24 13:46:10 +0800

    finish without user

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: true,
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
